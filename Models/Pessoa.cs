55808426d108a5f9c31711dd4cbd1fc085ac6876
Diana Avila <missernightangel@gmail.com>
2025-09-03 18:32:34 -0300

    criacao e classe Pessoa

---
+++ Models/Pessoa.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dio_net_POO.Models
+{
+    public class Pessoa
+    {
+        // Construtores
+        public Pessoa(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        // Propriedades
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        // Métodos
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using dio_net_POO.Models;
+
+Pessoa pessoa1 = new Pessoa("Vampiro", 330);
+pessoa1.Apresentar();
+
+Pessoa pessoa2 = new Pessoa("Zumbi", 25);
+pessoa2.Apresentar();
